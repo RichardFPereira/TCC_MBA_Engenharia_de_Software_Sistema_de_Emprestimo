430d3c679e5a0e06eb046edb95036e099e544883
RichardFPereira <richard.frei@hotmail.com>
2025-05-20 21:33:28 -0300

    Adicionar projeto UsuarioService com entidades Credencial e Usuario para cadastro e login

---
+++ Backend/UsuarioService/Models/Entities/Credencial.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.UsuarioService.Models.Entities
+{
+    public class Credencial
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLenth(100)]
+        public string Email { get; set; }
+        [Required]
+        public string SenhaHash { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataAlteracao { get; set; }
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+    }
+}
---
+++ Backend/UsuarioService/Models/Entities/Usuario.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.UsuarioService.Models.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+        [Required]
+        [StringLength(11)]
+        public string CPF { get; set; }        
+        public DateTime? DataNascimento { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataAlteracao { get; set; }
+    }
+}
